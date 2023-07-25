75fee3b05961c318ed439af3f43eac57b2ac1b34
TucanoMalicioso <137518798+TucanoMalicioso@users.noreply.github.com>
2023-07-25 17:12:48 -0300

    exercicio 9

---
+++ index8.js
@@ -0,0 +1,8 @@
+let a=5;
+let b=5;
+let c=1;
+let delta=0;
+
+delta=(b**2)-(4*a*c);
+
+console.log(delta);
---
+++ index9.js
@@ -0,0 +1,7 @@
+let v=0;
+let d=6;
+let r=d/2;
+
+v=4/3*r**3;
+
+console.log("O volume de uma esfera de raio "+r+" é "+ Math.round(v) +" PI");
